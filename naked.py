6fd3e8eb1404d83b73a1b31e8fd39d25e019ea2b
maris-o <maris.odzulis@va.lv>
2022-11-14 12:59:04 +0000

    Ierakstīju NASA API atslēgu

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "dCG28InXwffUE6TsflU9u0wZRUQEC5LTqcXenIbu"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
